cf7ea1a9ad6ed135cfc6b793d449fb4051dff54a
Brandon Wood <btwood@geometeor.com>
2014-11-09 16:16:05 -0500

    Made it such that only one image can be featured (at this time)

--- public/partials/fossils/single/images/view.php
+++ public/partials/fossils/single/images/view.php
@@ -5,9 +5,15 @@ function fossil_view_images( $fossil ) {
     $featured_image = array_pop( $images );
     $image_src = wp_get_attachment_url( $featured_image->ID );
     ?>
-    <h3>Images</h3>
-    <img id="fossil-featured-image" class="img-responsive" 
-            src="<?php echo $image_src ?>" style="max-width:100%;" />
+    <h3 class="sr-only">Image</h3>
+    <div class="activity-entry">
+        <div class="activity-body">
+            <img id="fossil-featured-image" class="img-responsive"
+                    src="<?php echo $image_src ?>" style="padding: 10px;" />
+        </div>
+    </div>
+    <?php
+    /*
     <?php if ( is_array( $images ) ): ?>
         <div class="fossil-images-small">
         <?php foreach ( $images as $image_post ): ?>
@@ -16,4 +22,5 @@ function fossil_view_images( $fossil ) {
         </div>
     <?php endif; ?>
     <?php
+    */
 }
