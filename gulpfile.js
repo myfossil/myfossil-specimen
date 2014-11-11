8099719b48b7f34d14f202a74b6da1bb9f3074a6
Brandon Wood <btwood@geometeor.com>
2014-11-11 13:05:28 -0500

    Simplified gulp tasks to have easier configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -5,87 +5,70 @@ var project   = 'myfossil';
 var gulp      = require('gulp')
   , gutil     = require('gulp-util')
   , plugins   = require('gulp-load-plugins')({ camelize: true })
-  , argv      = require('yargs').argv
-  , gulpif    = require('gulp-if')
   , lr        = require('tiny-lr')
   , server    = lr()
   , build     = './static/'
 ;
 
-gulp.task('init', function() {
-    if ( ! argv.production ) {
-    }
-});
+/**
+ * Paths to scripts that will ultimately become gulp tasks.
+ */
+var script_sets = {
+    'public-single': ['assets/src/js/public/single/*.js'],
+    'public-list': ['assets/src/js/public/list.js'],
+    'public-plugins': [
+            'assets/src/js/public/plugins/jquery-file-upload/' +
+                'jquery.ui.widget.js', 
+            'assets/src/js/public/plugins/**/*.js'
+        ],
+    'admin': ['assets/src/js/admin/*.js']
+};
 
-gulp.task('scripts-public-single', function() {
-  return gulp.src(['assets/src/js/public/single/*.js', '!assets/src/js/plugins.js'])
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.init()))
-      .pipe(gulpif( ! argv.production, plugins.jshint('.jshintrc')))
-      .pipe(plugins.concat('public-single.js'))
-      .pipe(gulp.dest('assets/staging'))
-      .pipe(plugins.rename({ suffix: '.min' }))
-      .pipe(gulpif(argv.production, plugins.uglify()))
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.write()))
-      .pipe(gulp.dest(build + '/js/'));
-});
+var scripts = [];
 
-gulp.task('scripts-public-list', function() {
-  return gulp.src(['assets/src/js/public/list.js', '!assets/src/js/plugins.js'])
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.init()))
-      .pipe(gulpif( ! argv.production, plugins.jshint('.jshintrc')))
-      .pipe(plugins.concat('public-list.js'))
-      .pipe(gulp.dest('assets/staging'))
-      .pipe(plugins.rename({ suffix: '.min' }))
-      .pipe(gulpif(argv.production, plugins.uglify()))
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.write()))
-      .pipe(gulp.dest(build + '/js/'));
-});
+for ( var set in script_sets ) {
+    ( function( set ) {
+        var gulp_task_name = 'scripts-' + set;
+        var filename = set + '.js';
 
-gulp.task('scripts-public-plugins', function() {
-  return gulp.src(['assets/src/js/public/plugins/jquery-file-upload/jquery.ui.widget.js', 'assets/src/js/public/plugins/**/*.js', '!assets/src/js/plugins.js'])
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.init()))
-      .pipe(gulpif( ! argv.production, plugins.jshint('.jshintrc')))
-      .pipe(plugins.concat('public-plugins.js'))
-      .pipe(gulp.dest('assets/staging'))
-      .pipe(plugins.rename({ suffix: '.min' }))
-      .pipe(gulpif(argv.production, plugins.uglify()))
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.write()))
-      .pipe(gulp.dest(build + '/js/'));
-});
+        scripts.push( gulp_task_name );
 
-gulp.task('scripts-public', ['scripts-public-single', 'scripts-public-list', 'scripts-public-plugins']);
+        gulp.task( gulp_task_name, 
+            function() {
+                return gulp.src( script_sets[set] )
+                  .pipe( plugins.sourcemaps.init() ) 
+                  .pipe( plugins.jshint( '.jshintrc' ) ) 
+                  .pipe( plugins.concat( filename ) )
+                  .pipe( gulp.dest( 'assets/staging' ) )
+                  .pipe( plugins.rename( { suffix: '.min' } ) )
+                  .pipe( plugins.uglify() )
+                  .pipe( plugins.sourcemaps.write() )
+                  .pipe( gulp.dest( build + '/js/') );
+            }
+        );
+    })( set );
 
-gulp.task('scripts-admin', function() {
-  return gulp.src(['assets/src/js/admin/*.js', '!assets/src/js/plugins.js'])
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.init()))
-      .pipe(gulpif( ! argv.production, plugins.jshint('.jshintrc')))
-      .pipe(plugins.concat('admin.js'))
-      .pipe(gulp.dest('assets/staging'))
-      .pipe(plugins.rename({ suffix: '.min' }))
-      .pipe(gulpif(argv.production, plugins.uglify()))
-      .pipe(gulpif( ! argv.production, plugins.sourcemaps.write()))
-      .pipe(gulp.dest(build + '/js/'));
-});
+}
 
-
-gulp.task('scripts', ['scripts-public', 'scripts-admin']);
+gulp.task('scripts', scripts );
 
 gulp.task('clean', function() {
-  return gulp.src(build+'**/.DS_Store', { read: false })
-      .pipe(plugins.clean());
+    return gulp.src(build+'**/.DS_Store', { read: false })
+            .pipe(plugins.clean());
 });
 
 gulp.task('watch', function() {
-  server.listen(35729, function (err) { // Listen on port 35729
-    if (err) {
-      return console.log(err)
-    };
-    gulp.watch('assets/src/js/**/*.js', ['scripts']);
-    gulp.watch('**/*.php').on('change', function(file) { 
-        plugins.livereload(server).changed(file.path); 
+    server.listen(35729, function (err) { // Listen on port 35729
+
+        gulp.watch('assets/src/js/**/*.js', ['scripts']);
+
+        gulp.watch('**/*.php').on('change', function(file) { 
+            plugins.livereload(server).changed(file.path); 
+        });
+
     });
-  });
+
 });
 
-gulp.task('build', ['init', 'scripts', 'clean']);
+gulp.task('build', ['scripts', 'clean']);
 gulp.task('default', ['build', 'watch']);
