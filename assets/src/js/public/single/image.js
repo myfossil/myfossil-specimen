27a56a4ca0e440fce0338197cd4f15a4a107eb18
Brandon Wood <btwood@geometeor.com>
2014-11-13 16:50:12 -0500

    Added basic loading element to image upload

--- assets/src/js/public/single/image.js
+++ assets/src/js/public/single/image.js
@@ -1,6 +1,25 @@
 ( function( $ ) {
     'use strict';
 
+    function status_loading() {
+        var img = $( 'img.fossil-image' );
+        var _parent = img.parent();
+
+        console.log( '__status_loading__' );
+        img.hide();
+        _parent.append( '<span class="loading"><i class="fa fa-spinner fa-spin"></i> Uploading... </span>');
+    }
+
+    function status_done() {
+        var img = $( 'img.fossil-image' );
+        var _parent = img.parent();
+        
+        console.log( '__status_done__' );
+
+        $( 'span.loading' ).hide();
+        img.show();
+    }
+
     $( function() {
         var post_id = $( '#post_id' ).val();
         var nonce = $( '#myfossil_specimen_nonce' ).val();
@@ -13,6 +32,9 @@
                 post_id: post_id,
             },
             url: ajaxurl,
+            start: function() {
+                status_loading();
+            },
             success: function( data ) {
                 if ( data && data.src ) 
                     $( 'img.fossil-image' ).attr( 'src', data.src );
@@ -20,6 +42,7 @@
             done: function( e, data ) {
                 console.info( e );
                 console.info( data );
+                status_done();
             },
             error: function( err ) {
                 console.error( err );
@@ -27,6 +50,7 @@
         });
 
         $( '#fossil-delete-image' ).click( function() {
+            
             $.ajax({
                 type: 'post',
                 url: ajaxurl,
