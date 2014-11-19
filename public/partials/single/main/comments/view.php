458c71cc7a2e9eb35e8f91db411d1fadebd9b51e
Brandon Wood <btwood@geometeor.com>
2014-11-19 12:41:26 -0500

    Added discussion section with activities and comments for the fossil

--- public/partials/single/main/comments/view.php
+++ public/partials/single/main/comments/view.php
@@ -3,7 +3,28 @@
 function fossil_view_comments( $fossil=null ) {
     ?>
 
-    <h3>Comments</h3>
+    <h3>Discussion</h3>
+
+    <?php if ( bp_has_activities( bp_ajax_querystring( 'activity&item_id=' . $fossil->id ) ) ) : ?>
+        <ul id="activity-stream" class="activity-list item-list">
+
+        <?php while (bp_activities()): ?>
+            <?php bp_the_activity(); ?>
+            <?php bp_get_template_part('activity/entry'); ?>
+        <?php endwhile; ?>
+
+        <?php if (bp_activity_has_more_items()): ?>
+            <li class="load-more">
+                <a href="<?php bp_activity_load_more_link() ?>"><?php _e('Load More', 'buddypress'); ?></a>
+            </li>
+        <?php endif; ?>
+
+        </ul>
+    <?php else: ?>
+        <div id="message" class="alert alert-info">
+            <p><?php _e('Sorry, there was no activity found. Please try a different filter.', 'buddypress'); ?></p>
+        </div>
+    <?php endif; ?>
 
     <?php
 }
