c5fbcf465401ee37deda3dcdc015bd0380f2b4dc
Brandon Wood <btwood@geometeor.com>
2014-12-15 14:24:22 -0500

    BUG: Removed erroneous events from rocketeer config

--- .rocketeer/events.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Rocketeer\Facades\Rocketeer;
-
-Rocketeer::before( 'deploy', array(
-    'composer self-update',
-    'composer install',
-    'npm install --cache',
-    'gulp build --production'
-));
