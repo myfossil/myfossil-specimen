eb35995206be8b93e2a39577fe1b22cf3dd1d121
Brandon Wood <btwood@geometeor.com>
2015-01-11 13:31:46 -0500

    ENH: Added basic image gallery/listing functionality

--- public/partials/single/images.php
+++ public/partials/single/images.php
@@ -4,7 +4,30 @@ require_once 'information/images.php';
 
 function myfossil_fossil_render_single_images( $fossil )
 {
-?>
-    <h3>Images</h3>
+    $images = get_attached_media( 'image', $fossil->id );
+
+    if ( ! is_array( $images ) ) {
+        $images = array( $images );
+    } 
+
+    ?> 
+        <div class="row"> 
+    <?php
+
+    foreach ( $images as $image ) {
+        $image_id = $image->ID;
+        $image_src = wp_get_attachment_url( $image_id );
+        ?>
+            <div class="activity-entry col-sm-12 col-md-4">
+                <div class="activity-body">
+                    <img class="img-responsive fossil-image"
+                            src="<?=$image_src ?>" style="padding: 10px;"
+                            data-attachment-id="<?=$image_id ?>" />
+                </div>
+            </div>
+        <?php
+    }
+    ?> 
+        </div>
     <?php
 }
