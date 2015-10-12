2fe95efd50127d3f2bb9a6a3d29c3a956420bf1f
Brandon Wood <woodb@users.noreply.github.com>
2015-10-11 20:35:56 -0400

    BUG: Removed irrelevant history from History
    Closes #233

--- public/partials/single/history.php
+++ public/partials/single/history.php
@@ -10,7 +10,7 @@ function myfossil_fossil_render_single_history( $fossil )
     $item_ids = array();
     foreach ( $item_id_keys as $key )
         if ( $fossil->{ $key } )
-        array_push( $item_ids, $fossil->{ $key } );
+            array_push( $item_ids, $fossil->{ $key } );
 
     $item_query = implode( ',', $item_ids );
 ?>
@@ -27,19 +27,22 @@ function myfossil_fossil_render_single_history( $fossil )
             </tr>
 
         <?php foreach ( $activities_template->activities as $activity ) : ?>
+            <?php if ( $activity->item_id == $fossil->id ): ?>
 
-            <tr>
-                <td>
-                    <?php echo get_avatar( $activity->user_id, 30 ) ?>
-                </td>
-                <td>
-                    <i class="fa fa-fw fa-clock-o"></i>
-                    <?php echo $activity->date_recorded ?>
-                </td>
-                <td>
-                    <?php echo $activity->action ?>
-                </td>
-            </tr>
+                <tr>
+                    <td>
+                        <?php echo get_avatar( $activity->user_id, 30 ) ?>
+                    </td>
+                    <td>
+                        <i class="fa fa-fw fa-clock-o"></i>
+                        <?php echo $activity->date_recorded ?>
+                    </td>
+                    <td>
+                        <?php echo $activity->action ?>
+                    </td>
+                </tr>
+
+            <?php endif; ?>
 
         <?php endforeach; ?>
 
